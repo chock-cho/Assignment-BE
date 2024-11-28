14d39c9ddd88dfdee4a27596488ccbb4187ef7b4
베뉴 <christer10@ewhain.net>
2024-11-29 03:17:45 +0900

    feat: [학생 api] 수업 신청 시 예외 처리 커스텀 어노테이션 구현
    - 현재 시각을 기준으로 30분 미만 남아있는 시점이 시작시각인 수업 신청에 대해 얘외처리 어노테이션 커스텀

--- src/main/java/ringle/backend/assignment/aspect/annotation/ReservationAspect.java
+++ src/main/java/ringle/backend/assignment/aspect/annotation/ReservationAspect.java
@@ -1,2 +1,39 @@
-package ringle.backend.assignment.aspect.annotation;public class ReservationAspect {
+package ringle.backend.assignment.aspect.annotation;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import ringle.backend.assignment.aspect.apiPayload.code.status.ErrorStatus;
+import ringle.backend.assignment.aspect.apiPayload.exception.handler.TempHandler;
+import ringle.backend.assignment.domain.Lecture;
+import ringle.backend.assignment.domain.mapping.Reservation;
+
+import java.time.LocalTime;
+
+
+@Aspect
+@Component
+@Slf4j
+public class ReservationAspect {
+    @Before("@annotation(validateReservation) && args(reservation, ..)")
+    public void validateReservationAvailability(JoinPoint joinPoint, ValidateReservation validateReservation, Reservation reservation) {
+        Lecture lecture = reservation.getLecture();
+
+        if(lecture == null)
+            throw new TempHandler(ErrorStatus.RESERVATION_NOT_FOUND); // 수업 예약 요청에 해당하는 가능한 타임 슬롯이 없음
+
+        LocalTime classTime = lecture.toLocalTime(); // 열려 있는 타임 슬롯
+        LocalTime cutoffTime = classTime.minusMinutes(30); // cutOffTime: 현재 요청 시간 수업 시작 30분 전
+
+        LocalTime requestTime = reservation.getCreatedAt().toLocalTime();
+
+        if (requestTime.isAfter(cutoffTime))
+            throw new TempHandler(ErrorStatus.RESERVATION_FORBIDDEN);
+        // 현재 시각을 기준으로 30분 미만 남아있는 시점에서 수업 예약 요청이라면, 실패
+
+        log.info("예약이 가능합니다. [Lecture ID: {}, Request Time: {}]", reservation.getLecture().getId(), requestTime);
+    }
 }
--- src/main/java/ringle/backend/assignment/aspect/annotation/ValidateReservation.java
+++ src/main/java/ringle/backend/assignment/aspect/annotation/ValidateReservation.java
@@ -1,2 +1,12 @@
-package ringle.backend.assignment.aspect.annotation;public interface CheckReservation {
+package ringle.backend.assignment.aspect.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidateReservation {
+    // 예약 요청 시각을 기준으로, 해당 Lecture의 예약 가능 여부를 확인하는 커스텀 어노테이션
 }
