d37fd50d88e1bfe0676c522860af30cb30348ee5
베뉴 <christer10@ewhain.net>
2024-12-01 01:08:06 +0900

    refact: converter, service 책임 명확히 분리
    - Converter은 데이터↔ dto "변환" 해주는 로직에 집중
    - Service는 비즈니스 로직에 집중
    - SRP(단일 책임 원칙) 개선

--- src/main/java/ringle/backend/assignment/converter/LectureConverter.java
+++ src/main/java/ringle/backend/assignment/converter/LectureConverter.java
@@ -18,9 +18,6 @@ import java.util.stream.Collectors;
 @Component
 @AllArgsConstructor
 public class LectureConverter {
-
-    private final LectureRepository lectureRepository;
-
     public List<LectureResponseDto.LectureCreateResponse> toLectureResponseDtos(List<Lecture> lectures) {
         return lectures.stream()
                 .map(this::toLectureResponseDto)
@@ -43,37 +40,6 @@ public class LectureConverter {
         return LectureResponseDto.LectureDeleteResponse.builder()
                 .lectureId(lectureId)
                 .message(message)
-                .build();
-    }
-
-    // lecture 엔티티 -> LectureGetResponse DTO로 전환
-    public LectureResponseDto.LectureGetResponse toLectureGetResponse(Lecture lecture, Lecture previousLecture, Lecture nextLecture) {
-        LectureResponseDto.LectureGetResponse.TimeSlotInfo previousSlotInfo = new LectureResponseDto.LectureGetResponse.TimeSlotInfo(
-                previousLecture != null ? previousLecture.getStartTimeSlot().name() : "N/A",
-                previousLecture != null && previousLecture.isAvailable(),
-                Objects.requireNonNull(previousLecture).getId()
-        );
-
-        LectureResponseDto.LectureGetResponse.TimeSlotInfo currentSlotInfo = new LectureResponseDto.LectureGetResponse.TimeSlotInfo(
-                lecture.getStartTimeSlot().name(),
-                lecture.isAvailable(),
-                Objects.requireNonNull(previousLecture).getId()
-        );
-
-        LectureResponseDto.LectureGetResponse.TimeSlotInfo nextSlotInfo = new LectureResponseDto.LectureGetResponse.TimeSlotInfo(
-                nextLecture != null ? nextLecture.getStartTimeSlot().name() : "N/A",
-                nextLecture != null && nextLecture.isAvailable(),
-                Objects.requireNonNull(previousLecture).getId()
-        );
-
-        return LectureResponseDto.LectureGetResponse.builder()
-                .lectureId(lecture.getId())
-                .tutorId(lecture.getTutor().getId())
-                .tutorName(lecture.getTutor().getName())
-                .date(String.valueOf(lecture.getDate()))
-                .timeSlots(new LectureResponseDto.LectureGetResponse.TimeSlots
-                        (previousSlotInfo, currentSlotInfo, nextSlotInfo))
-                .isAvailable(lecture.isAvailable())
                 .build();
     }
 
--- src/main/java/ringle/backend/assignment/converter/ReservationConverter.java
+++ src/main/java/ringle/backend/assignment/converter/ReservationConverter.java
@@ -10,6 +10,8 @@ import ringle.backend.assignment.domain.enums.LectureType;
 import ringle.backend.assignment.domain.mapping.Reservation;
 import ringle.backend.assignment.service.ReservationService.ReservationService;
 
+import java.util.List;
+
 @Component
 public class ReservationConverter {
 
@@ -28,6 +30,39 @@ public class ReservationConverter {
                 .reservationId(reservation.getId())
                 .reservedLectureType(String.valueOf(reservation.getLectureType()))
                 .message("예약이 성공적으로 완료되었습니다.")
+                .build();
+    }
+
+    // 신청한 수업에 대한 예약 내역 조회
+
+    public List<ReservationResponseDto.ReservationInfoResponse> toReservationInfoResponses(List<Reservation> reservations) {
+        return reservations.stream()
+                .map(this::toReservationInfoResponse)
+                .toList();
+    }
+
+    public ReservationResponseDto.ReservationInfoResponse toReservationInfoResponse(Reservation reservation) {
+        String reservedLectureType = reservation.getLectureType().name();
+        ReservationResponseDto.ReservationInfoResponse.LectureInfo lectureInfo = ReservationResponseDto.ReservationInfoResponse.LectureInfo.builder()
+                .startLecture(toLectureDetail(reservation.getLectureId1()))
+                .endLecture("_30_MIN".equals(reservedLectureType) ? null : toLectureDetail(reservation.getLectureId2()))
+                .build();
+
+        return ReservationResponseDto.ReservationInfoResponse.builder()
+                .reservationId(reservation.getId())
+                .reservedLectureType(reservedLectureType)
+                .tutorInfo(ReservationResponseDto.ReservationInfoResponse.TutorInfo.builder()
+                        .tutorId(reservation.getTutor().getId())
+                        .tutorName(reservation.getTutor().getName())
+                        .build())
+                .lectureInfo(lectureInfo)
+                .build();
+    }
+
+    private ReservationResponseDto.ReservationInfoResponse.LectureDetail toLectureDetail(Lecture lecture) {
+        return ReservationResponseDto.ReservationInfoResponse.LectureDetail.builder()
+                .lectureId(lecture.getId())
+                .lectureTimeSlot(lecture.getStartTimeSlot().name())
                 .build();
     }
 }
